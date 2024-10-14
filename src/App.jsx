e046bf1d6c83c071757500b759faab535ab9f90d
omoro <paulomoro20@gmail.com>
2024-10-14 11:28:04 +0100

    safe tasks into local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import TodoForm from './component/TodoForm'
 import './App.css'
 import Header from './component/Header'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Task from './component/Task'
 
 function App() {
 
   const [inputData, setInputData] = useState("")
 
-  const [tasks, setTasks] = useState([])
+
+  const [tasks, setTasks] = useState(()=>{
+    const savedTasks  = localStorage.getItem('tasks')
+    return savedTasks ? JSON.parse(savedTasks) : []
+  })
 
   const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(()=>{
+     localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
 
 
   function handleMode(){
     setDarkMode(prevMode => !prevMode)
   }
  
+
+
+
   return (
    <div className={darkMode ? "main dark" :  "main"}>
       <Header 
--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,12 +1,16 @@
 
 export default function Task(prop){
 
+    
+
 
     return(
         <div className="task">
             <ol>
                 {prop.tasks.map((task,index)=>(
-                    <li key={index}>{task} </li>
+                    <>
+                    <li key={index} id={`task-${index}`}> {task} </li>
+                    </>
                 ))}
             </ol>
         </div>
